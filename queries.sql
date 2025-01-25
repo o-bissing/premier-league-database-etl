58eb59cce023c5edceba66806ebe8af199951a39
o-bissing <oleg.bissing@gmail.com>
2025-01-25 18:12:06 +0100

    standings table update

--- queries.sql
+++ queries.sql
@@ -1,10 +1,11 @@
 -- 0. Query to get Standings for the season
 SELECT 
     s.table_position AS Position,
-    t.team_name AS Team
+    t.team_name AS Team,
+    s.points AS Points
 FROM standings s
 JOIN teams t ON s.team_id = t.team_id
-GROUP BY t.team_name, s.table_position
+GROUP BY t.team_name, s.table_position, s.points
 ORDER BY s.table_position ASC;
 
 -- 1. Query to find Top-Scoring Stadiums
--- test_query.sql
+++ test_query.sql
@@ -1,7 +1,8 @@
 SELECT 
     s.table_position AS Position,
-    t.team_name AS Team
+    t.team_name AS Team,
+    s.points AS Points
 FROM standings s
 JOIN teams t ON s.team_id = t.team_id
-GROUP BY t.team_name, s.table_position
+GROUP BY t.team_name, s.table_position, s.points
 ORDER BY s.table_position ASC;
